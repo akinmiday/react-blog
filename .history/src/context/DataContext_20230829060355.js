4df17c5cb4affac3eeeeb1a0608499db8dfa3241
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-08-29 13:52:37 +0100

    data context

---
+++ .history/src/context/DataContext_20230829060355.js
@@ -0,0 +1,12 @@
+import { createContext, useState, useEffect } from "react";
+
+const DataContext = createContext();
+
+export const DataProvider = ({ children }) => {
+  return(
+    <DataContext.Provider value={{
+
+    }}
+        {}
+  )
+};
